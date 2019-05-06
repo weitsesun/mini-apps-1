076b5ce9eeeddada56e2042f44e467ed5f8a9b88
Wayne Sun <wtse.sun@gmail.com>
2019-05-06 13:21:19 -0700

    implement reset function on index.html and app.js

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -5,12 +5,15 @@ console.log("loaded app.js");
 console.log(document.getElementsByClassName('mybutton'));
 
 
-(function handleClick(e) {
+(function handleClick() {
   //change e.target.innerText
   
   var buttons = document.getElementsByClassName('mybutton');
     for (var i = 0; i < buttons.length; i++) {
       buttons[i].onclick = function(e) {
+        
+        var winCases = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+
         if(!e.target.innerText) {
           var count = 0;
           var curTic = document.getElementsByClassName('mybutton');
@@ -20,6 +23,18 @@ console.log(document.getElementsByClassName('mybutton'));
           var change = ['x', 'o'];
           this.innerText = change[count%2];
         }
+      
+      //check win cases:
+        //iterator through the table
+      
+      
       }
     }
 })()
+
+function reset() {
+  var buttons = document.getElementsByClassName('mybutton');
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].innerText = "";
+    }
+}
