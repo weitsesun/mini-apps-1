4b6374aac246f3559a38e1b99fb4c863d53b8ee6
Wayne Sun <wtse.sun@gmail.com>
2019-05-06 12:19:31 -0700

    Implement handle click function to toggle x and o on buttons

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -5,8 +5,21 @@ console.log("loaded app.js");
 console.log(document.getElementsByClassName('mybutton'));
 
 
-function handleClick(e) {
-  //change innerText
-  var toggle = document.getElementsByClassName('mybutton');
-  console.log()
-}
+(function handleClick(e) {
+  //change e.target.innerText
+  
+  var buttons = document.getElementsByClassName('mybutton');
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].onclick = function(e) {
+        if(!e.target.innerText) {
+          var count = 0;
+          var curTic = document.getElementsByClassName('mybutton');
+          for (var i = 0; i < curTic.length; i++){
+            count+=curTic[i].innerText.length;
+          }
+          var change = ['x', 'o'];
+          this.innerText = change[count%2];
+        }
+      }
+    }
+})()
