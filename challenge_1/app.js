d39decd7d0d4a78ec4fab61643ee0c18f40d597f
Wayne Sun <wtse.sun@gmail.com>
2019-05-06 11:39:32 -0700

    created css file and script to html

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -4,3 +4,9 @@ console.log("loaded app.js");
 
 console.log(document.getElementsByClassName('mybutton'));
 
+
+function handleClick(e) {
+  //change innerText
+  var toggle = document.getElementsByClassName('mybutton');
+  console.log()
+}
